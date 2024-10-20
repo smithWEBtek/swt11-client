fcb41110b08cfbeb0e0dd98d26540bce6cd3b63b
brad@smithwebtek.com <brad@smithwebtek.com>
2024-10-20 12:02:04 -0400

    Base react app components

---
+++ src/components/MainCanvas/MainCanvas.js
@@ -0,0 +1,15 @@
+import './MainCanvas'
+import '../../styles.css'
+
+const MainCanvas = () => {
+  return (
+    <section className='maincanvas-container'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum impedit provident beatae ex! Labore aut a deleniti blanditiis quos sint quas molestias quaerat necessitatibus sunt temporibus, vitae at harum reiciendis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum impedit provident beatae ex! Labore aut a deleniti blanditiis quos sint quas molestias quaerat necessitatibus sunt temporibus, vitae at harum reiciendis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum impedit provident beatae ex! Labore aut a deleniti blanditiis quos sint quas molestias quaerat necessitatibus sunt temporibus, vitae at harum reiciendis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum impedit provident beatae ex! Labore aut a deleniti blanditiis quos sint quas molestias quaerat necessitatibus sunt temporibus, vitae at harum reiciendis.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum impedit provident beatae ex! Labore aut a deleniti blanditiis quos sint quas molestias quaerat necessitatibus sunt temporibus, vitae at harum reiciendis.</p>
+    </section>)
+}
+
+export default MainCanvas;
