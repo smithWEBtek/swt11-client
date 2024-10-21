1eef98de74018d913c0665f25275772eeee82f60
brad@smithwebtek.com <brad@smithwebtek.com>
2024-10-21 03:14:46 -0400

    Pages,json,routes,structure working

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 const HeaderNav = () => {
   return (
     <div className='headernav'>
-      <Link to='/' className='headernav-link'>about</Link>
-      <Link to='/' className='headernav-link'>blog</Link>
-      <Link to='/' className='headernav-link'>samples</Link>
-      <Link to='/' className='headernav-link'>library</Link>
+      <Link to='/about' className='headernav-link'>about</Link>
+      <Link to='/blog' className='headernav-link'>blog</Link>
+      <Link to='/samples' className='headernav-link'>samples</Link>
+      <Link to='/library' className='headernav-link'>library</Link>
     </div>)
 }
 
--- src/components/SideContent/SideContent.js
+++ src/components/SideContent/SideContent.js
@@ -1,12 +1,13 @@
-const SideContent = ({ title, body }) => {
+const SideContent = ({ title, body, json }) => {
   return (
     <aside className="w-64 bg-gray-800 text-white flex-shrink-0 sidecontent">
       <div className="p-4">
-        <h2 className="text-xl font-bold sidecontent-title">SideContent title</h2>
+        <h2 className="text-xl font-bold sidecontent-title">{title}</h2>
       </div>
       <nav className="p-4">
-        <div className="sidecontent-body">SideContent body</div>
+        <div className="sidecontent-body">{body}</div>
       </nav>
+      <img src={`../assets/${json.resources.images[3]}`} alt='asdf' />
     </aside>
   )
 }
